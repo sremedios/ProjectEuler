a6574a1cbae2b2b5d68c9e99ae8dc22042512341
sremedios <sam.remedios@gmail.com>
2016-12-09 10:16:13 -0600

    removed extra lines

--- python/006_Sum_square_difference.py
+++ python/006_Sum_square_difference.py
@@ -1,8 +1,6 @@
 from math import sqrt
 
 def sumSquareDif(top):
-    
-    
     sumSq = sum([x**2 for x in range(1, top+1)])
     sqSum = sum([x for x in range(1, top+1)])**2
     return sqSum - sumSq
